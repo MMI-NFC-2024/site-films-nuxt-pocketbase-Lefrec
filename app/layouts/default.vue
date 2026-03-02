5ea54af35c8e3b06d3c193de32c1a9d65a05cf9e
Lefrec <blackfox244@gmail.com>
2026-03-02 14:16:46 +0100

    code de base avec type router

---
+++ app/layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <nav>
+        <NuxtLink to="/">Accueil</NuxtLink>
+        <NuxtLink to="/films">Films</NuxtLink>
+    </nav>
+    <slot></slot>
+  </div>
+</template>
---
+++ app/pages/films/index.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <h1>Liste des films</h1>
+</template>
