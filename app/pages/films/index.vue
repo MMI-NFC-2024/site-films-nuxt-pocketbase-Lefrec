df574b8a530d67e21663b86befea46fbc1283528
Lefrec <blackfox244@gmail.com>
2026-03-02 15:03:39 +0100

    listing de films

--- app/pages/films/index.vue
+++ app/pages/films/index.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts">
-
+    const nuxtApp = useNuxtApp();
+    const films = await nuxtApp.$pb.collection("film").getFullList();
 </script>
 
 <template>
     <h1 class="text-4xl">Liste des films</h1>
+    <ul>
+        <li v-for="film in films">{{ film.titre }}</li>
+    </ul>
 </template>
