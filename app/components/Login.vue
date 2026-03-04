f9ee0c808f2c52471df0e9c6ae95fd148e0de905
Lefrec <blackfox244@gmail.com>
2026-03-04 11:48:21 +0100

    oauth login

---
+++ app/components/Login.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+    const nuxtApp = useNuxtApp();
+    async function loginGitHub() {
+        const authData = await nuxtApp.$pb.collection("users").authWithOAuth2({provider: "github"})
+        console.log("[loginGitHub] authData : ",authData)
+    }
+    const user = nuxtApp.$user;
+</script>
+
+<template>
+    <button v-if="user" @click="nuxtApp.$pb.authStore.clear">Se déconnecter {{ user?.name || user?.email }}</button>
+    <button v-else @click="loginGitHub">Se connecter avec GitHub</button>
+</template>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -4,6 +4,7 @@
         <NuxtLink to="/">Accueil</NuxtLink>
         <NuxtLink to="/films">Films</NuxtLink>
     </nav>
+    <Login/>
     <slot></slot>
   </div>
 </template>
